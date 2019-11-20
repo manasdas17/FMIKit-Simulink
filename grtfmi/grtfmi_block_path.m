eef750b4ad6d179c3da050ac55f83e7229529472
Torsten Sommer <torsten.sommer@3ds.com>
2019-11-20 22:44:04 +0100

    Escape non-ASCII characters in variable names
    see #99

--- grtfmi/grtfmi_block_path.m
+++ grtfmi/grtfmi_block_path.m
@@ -11,10 +11,10 @@ while j <= numel(p)
       j = j + 2;
       continue
     end
-    segments{end+1} = p(i+1:j-1);
+    segments{end+1} = p(i+1:j-1); %#ok<AGROW>
     i = j;
   elseif j == numel(p)
-    segments{end+1} = p(i+1:j);
+    segments{end+1} = p(i+1:j); %#ok<AGROW>
   end
   j = j + 1;
 end
@@ -22,18 +22,30 @@ end
 variable_name = '';
 
 for i = 2:numel(segments)
+  
   segment = segments{i};
+  
   segment = strrep(segment, '//', '/');
   segment = strrep(segment, '&', '&amp;');
-  if regexp(segment, '\s')
-    segment = ['&quot;' segment '&quot;'];
+  
+  % escape non-ASCII characters
+  nonascii = segment(segment > 127);
+  for j = 1:numel(nonascii)
+    c = nonascii(j);
+    segment = strrep(segment, c, ['&#x' dec2hex(c) ';']); 
   end
-
+  
+  % add quotes if the segment contains spaces
+  if regexp(segment, '\s')
+    segment = ['&quot;' segment '&quot;']; %#ok<AGROW>
+  end
+  
   if isempty(variable_name)
     variable_name = segment;
   else
-    variable_name = [variable_name '.' segment];
+    variable_name = [variable_name '.' segment]; %#ok<AGROW>
   end
+  
 end
 
 end
