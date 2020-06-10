a90a5d8ccf9984cac63d4215a2d0b99db1796cbd
Torsten Sommer <torsten.sommer@3ds.com>
2020-06-11 00:33:27 +0200

    Use CustomInclude and CustomLibrary when building generated S-function
    see #172

--- +FMIKit/private/applyDialog.m
+++ +FMIKit/private/applyDialog.m
@@ -6,6 +6,9 @@ function applyDialog(dialog)
 userData = userDataToStruct(dialog.getUserData());
 set_param(dialog.blockHandle, 'UserData', userData, 'UserDataPersistent', 'on');
 
+% set the S-function parameters
+FMIKit.setSFunctionParameters(dialog.blockHandle)
+
 if userData.useSourceCode
 
     % generate the S-function source
@@ -14,23 +17,35 @@ if userData.useSourceCode
     model_identifier = char(dialog.getModelIdentifier());
     unzipdir = char(dialog.getUnzipDirectory());
 
-    pkginfo = what('FMIKit');
-    [fmikitdir, ~, ~] = fileparts(pkginfo.path);
-
     % build the S-function
     clear(['sfun_' model_identifier])
 
     disp(['Compiling S-function ' model_identifier])
+    
+    mex_args = {};
+              
+    % custom sources
+    custom_include = get_param(gcs, 'CustomInclude');
+    custom_include = split_paths(custom_include);
+    for i = 1:numel(custom_include)
+        mex_args{end+1} = ['-I"' custom_include{i} '"'];
+    end
+    
+    % custom libraries
+    custom_library = get_param(gcs, 'CustomLibrary');
+    custom_library = split_paths(custom_library);
+    for i = 1:numel(custom_library)
+        mex_args{end+1} = ['"' custom_library{i} '"'];
+    end
+    
+    % S-function source
+    mex_args{end+1} = ['sfun_' model_identifier '.c'];
 
-    mex_args = {['sfun_' model_identifier '.c'], ...
-                ['-I' fullfile(fmikitdir, 'include')], ...
-                ['-I' fullfile(unzipdir, 'sources')], ...
-                '-lshlwapi'};
-
+    % FMU sources
     it = dialog.getSourceFiles().listIterator();
 
     while it.hasNext()
-        mex_args{end+1} = fullfile(unzipdir, 'sources', it.next());
+        mex_args{end+1} = ['"' fullfile(unzipdir, 'sources', it.next()) '"'];
     end
 
     try
@@ -43,6 +58,47 @@ if userData.useSourceCode
 
 end
 
-FMIKit.setSFunctionParameters(dialog.blockHandle)
+end
+
+
+function l = split_paths(s)
+% split a list of space separated and optionally quoted paths into
+% a cell array of strings 
+
+s = [s ' ']; % append a space to catch the last path
+
+l = {};  % path list
+
+p = '';     % path
+q = false;  % quoted path
+
+for i = 1:numel(s)
+  
+    c = s(i); % current character
+  
+    if q
+        if c == '"'
+            q = false;
+            if ~isempty(p)
+                l{end+1} = p;
+            end
+            p = '';
+        else
+            p(end+1) = c;
+        end
+        continue
+    end
+      
+    if c == '"'
+        q = true;
+    elseif c == ' '
+        if ~isempty(p)
+            l{end+1} = p;
+        end
+        p = '';
+    else
+        p(end+1) = c;
+    end
+end
 
 end
